4fb1c54bcce8c08748652a055d8832d5186bf856
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2022-01-06 12:26:23 +0100

    added Javadoc

--- raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/model/Product.java
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/model/Product.java
@@ -8,6 +8,14 @@ public class Product {
 	private long id;
 	private static long lastId = 0;
 
+	
+	/**
+	 * Constructs a new Product with the specified name, price and quantity available. Sets its id to a positive incremental number.
+	 * @param name of the product
+	 * @param price of the product
+	 * @param availableQuantity of the product
+	 * @throws IllegalArgumentException if the specified name is empty or null, price is negative and quantity is negative 
+	 */
 	public Product(String name, double price, int availableQuantity) {
 
 		if (!(name != null && !name.trim().isEmpty())) {
@@ -25,6 +33,7 @@ public class Product {
 		this.id = ++lastId;
 	}
 
+	
 	private void setAvailableQuantityValidation(int availableQuantity) {
 		if (availableQuantity < 0) {
 			throw new IllegalArgumentException("Negative available quantity: " + availableQuantity);
@@ -33,23 +42,43 @@ public class Product {
 	}
 
 
+	/**
+	 * Returns the price
+	 * @return the price
+	 */
 	public double getPrice() {
 		return price;
 	}
 
+	/**
+	 * Returns the available quantity
+	 * @return the available quantity
+	 */
 	public int getAvailableQuantity() {
 		return availableQuantity;
 	}
 
+	/**
+	 * Returns the name
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Returns the id
+	 * @return the id
+	 */
 	public long getId() {
 		return id;
 	}
 
-	
+	/**
+	 * Sets available quantity to the specified quantity.
+	 * @param availableQuantity to be set
+	 * @throws IllegalArgumentException if the specified availableQuantity is negative 
+	 */
 	public void setAvailableQuantity(int availableQuantity) {
 		setAvailableQuantityValidation(availableQuantity);
 	}
