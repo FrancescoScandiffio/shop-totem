983c03feea5ac664f7c1b6ebdc3c7fb796c22fdd
FrancescoScandiffio <francesco.scandiffio@stud.unifi.it>
2022-01-10 18:59:06 +0100

    refactor(Order): refactor method 'insertItem'
    replace the while loop with a stream filter

--- raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/model/Order.java
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/model/Order.java
@@ -11,14 +11,14 @@ public class Order {
 	}
 
 	public void insertItem(Product product, int quantity) throws NullPointerException {
-		int index = 0;
-		while (index < items.size() && items.get(index).getProduct().getId() != product.getId())
-			index++;
-
-		if (index == items.size())
+		OrderItem item = items.stream()
+				.filter(obj -> obj.getProduct().getId() == product.getId())
+				.findFirst()
+				.orElse(null);
+		if (item == null)
 			items.add(new OrderItem(product, quantity));
 		else
-			items.get(index).increaseQuantity(quantity);
+			item.increaseQuantity(quantity);
 	}
 
 }
