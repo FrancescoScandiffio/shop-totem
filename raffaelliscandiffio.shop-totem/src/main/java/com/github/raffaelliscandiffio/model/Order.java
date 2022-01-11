a744fdd515b1b998753b2e73ae57b1c70793a2f5
FrancescoScandiffio <francesco.scandiffio@stud.unifi.it>
2022-01-11 18:13:33 +0100

    doc(Order): add javadoc comments to 'insertItem'

--- raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/model/Order.java
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/model/Order.java
@@ -10,9 +10,16 @@ public class Order {
 		this.items = items;
 	}
 
-	public void insertItem(Product product, int quantity) throws NullPointerException {
-		OrderItem item = items.stream()
-				.filter(obj -> obj.getProduct().getId() == product.getId())
+	/**
+	 * Encapsulate the specified product and quantity in a new OrderItem instance. 
+	 * If the product is already binded to an item, increase its quantity.
+	 * @param product the product to be added to the order
+	 * @param the quantity of product to be added to the order
+	 * @throws NullPointerException if product is null
+	 * @throws IllegalArgumentException if quantity is non-positive
+	 */
+	public void insertItem(Product product, int quantity) throws NullPointerException, IllegalArgumentException {
+		OrderItem item = items.stream().filter(obj -> obj.getProduct().getId() == product.getId())
 				.findFirst()
 				.orElse(null);
 		if (item == null)
@@ -20,5 +27,6 @@ public class Order {
 		else
 			item.increaseQuantity(quantity);
 	}
+
 
 }
