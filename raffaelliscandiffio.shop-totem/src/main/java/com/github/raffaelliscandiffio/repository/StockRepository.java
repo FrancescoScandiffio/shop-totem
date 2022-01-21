9dc675ce8062862cfc119ee92db6cf00be27d540
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2022-01-21 18:18:42 +0100

    feat(Repositories): added stock and product repository interfaces

---
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/repository/StockRepository.java
@@ -0,0 +1,11 @@
+package com.github.raffaelliscandiffio.repository;
+
+import com.github.raffaelliscandiffio.model.Stock;
+
+public interface StockRepository {
+	
+	public Stock findById(long id);
+
+	public void save(Stock stock);
+
+}
