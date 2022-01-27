b5fba7192e92bc7a31d1d8a8107ef2cd84ff27c5
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2022-01-27 10:52:57 +0100

    feat(ProductMySQLRepository): add generated class from ProductRepository

---
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/repository/mysql/ProductMySQLRepository.java
@@ -0,0 +1,28 @@
+package com.github.raffaelliscandiffio.repository.mysql;
+
+import java.util.List;
+
+import com.github.raffaelliscandiffio.model.Product;
+import com.github.raffaelliscandiffio.repository.ProductRepository;
+
+public class ProductMySQLRepository implements ProductRepository{
+
+	@Override
+	public List<Product> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
