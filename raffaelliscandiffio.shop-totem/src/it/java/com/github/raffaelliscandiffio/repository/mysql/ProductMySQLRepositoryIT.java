1ff4b858445d8b9f144eaeaadf440da3a03d1521
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2022-01-31 17:29:58 +0100

    feat(ProductMySQLRepository): add 'findById' implementation

--- raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/repository/mysql/ProductMySQLRepositoryIT.java
+++ raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/repository/mysql/ProductMySQLRepositoryIT.java
@@ -1,6 +1,9 @@
 package com.github.raffaelliscandiffio.repository.mysql;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.NoSuchElementException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,6 +19,7 @@ import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import com.github.raffaelliscandiffio.model.Product;
 
 @Testcontainers
 class ProductMySQLRepositoryIT {
@@ -29,6 +33,7 @@ class ProductMySQLRepositoryIT {
 	@Container
 	public static final MySQLContainer mysqlContainer = new MySQLContainer("mysql:8.0.28")
 			.withDatabaseName(TOTEM_DB_NAME).withUsername("mysql").withPassword("mysql");
+
 
 	@BeforeAll
 	public static void createEntityManagerFactory() {
@@ -44,7 +49,12 @@ class ProductMySQLRepositoryIT {
 	@BeforeEach
 	void setup() {
 		entityManager = emf.createEntityManager();
+		// always starting with empty database
+		entityManager.getTransaction().begin();
+        entityManager.createQuery("DELETE FROM Product").executeUpdate();
+        entityManager.getTransaction().commit();
 		productRepository = new ProductMySQLRepository(entityManager);
+		
 	}
 
 	@AfterEach
@@ -52,16 +62,32 @@ class ProductMySQLRepositoryIT {
 		if (entityManager.getTransaction().isActive()) {
 			entityManager.getTransaction().rollback();
 		}
-
 		if (entityManager.isOpen()) {
 			entityManager.close();
-		}
+		}	
 	}
 	
 	@Test
-	@DisplayName("Test container is running")
-	void testContainerIsRunning() {
-		assertThat(mysqlContainer.isRunning()).isTrue();
+	@DisplayName("'findById' when the id is found")
+	void testFindByIdWhenIdIsFound() {
+		Product product1 = new Product(1, "pizza", 5.5);
+		Product product2 = new Product(2, "pasta", 2.3);
+		addTestProductToDatabase(product1); 
+		addTestProductToDatabase(product2); 
+		assertThat(productRepository.findById(2)).isEqualTo(product2);
+	}
+
+	@Test
+	@DisplayName("'findById' when the id is not found should throw NoSuchElementException")
+	void testFindByIdWhenIdIsNotFoundShouldThrowNoSuchElementException() {
+		assertThatThrownBy(() -> productRepository.findById(1)).isInstanceOf(NoSuchElementException.class)
+				.hasMessage("Product with id 1 not found");
+	}
+	
+	private void addTestProductToDatabase(Product product) {
+		entityManager.getTransaction().begin();
+		entityManager.persist(product);
+		entityManager.getTransaction().commit();
 	}
 
 }
--- raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/repository/mysql/ProductMySQLRepository.java
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/repository/mysql/ProductMySQLRepository.java
@@ -1,6 +1,7 @@
 package com.github.raffaelliscandiffio.repository.mysql;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import javax.persistence.EntityManager;
 
@@ -9,7 +10,7 @@ import com.github.raffaelliscandiffio.repository.ProductRepository;
 
 public class ProductMySQLRepository implements ProductRepository{
 	
-	private EntityManager entityManager;
+    private EntityManager entityManager;
 	
 	public ProductMySQLRepository(EntityManager entityManager) {
 		this.entityManager = entityManager;
@@ -23,8 +24,11 @@ public class ProductMySQLRepository implements ProductRepository{
 
 	@Override
 	public Product findById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Product product = entityManager.find(Product.class, id);
+		if (product != null)
+			return product;
+		else
+			throw new NoSuchElementException(String.format("Product with id %d not found", id));
 	}
 
 	@Override
