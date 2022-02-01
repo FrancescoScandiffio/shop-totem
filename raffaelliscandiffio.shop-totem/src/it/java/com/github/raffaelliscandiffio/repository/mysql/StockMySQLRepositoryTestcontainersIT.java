5288af7ba15f83b9ff4f28cc0039c021c180a7c6
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2022-02-01 18:12:49 +0100

    feat(StockMySQLRepository): add non implemented class

---
+++ raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/repository/mysql/StockMySQLRepositoryTestcontainersIT.java
@@ -0,0 +1,7 @@
+package com.github.raffaelliscandiffio.repository.mysql;
+
+
+class StockMySQLRepositoryTestcontainersIT {
+
+
+}
---
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/repository/mysql/StockMySQLRepository.java
@@ -0,0 +1,26 @@
+package com.github.raffaelliscandiffio.repository.mysql;
+
+import com.github.raffaelliscandiffio.model.Stock;
+import com.github.raffaelliscandiffio.repository.StockRepository;
+
+public class StockMySQLRepository implements StockRepository {
+
+	@Override
+	public Stock findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Stock stock) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Stock stock) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
