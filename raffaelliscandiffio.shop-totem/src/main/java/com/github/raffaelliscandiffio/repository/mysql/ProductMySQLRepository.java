86bb75422e4533e6b75169d690804e91bde699c7
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2022-02-01 16:18:47 +0100

    feat(ProductMySQLRepository): remove check on already existing id 'save'

--- raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/repository/mysql/ProductMySQLRepository.java
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/repository/mysql/ProductMySQLRepository.java
@@ -2,22 +2,14 @@ package com.github.raffaelliscandiffio.repository.mysql;
 
 import java.util.List;
 
-import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
-
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import com.github.raffaelliscandiffio.model.Product;
 import com.github.raffaelliscandiffio.repository.ProductRepository;
-import com.github.raffaelliscandiffio.utils.LogUtility;
 
 public class ProductMySQLRepository implements ProductRepository{
 	
     private EntityManager entityManager;
-    private static final Logger LOGGER = LogManager.getLogger(ProductMySQLRepository.class);
-	private static final LogUtility logUtil = new LogUtility();
 	
 	public ProductMySQLRepository(EntityManager entityManager) {
 		this.entityManager = entityManager;
@@ -38,13 +30,8 @@ public class ProductMySQLRepository implements ProductRepository{
 
 	@Override
 	public void save(Product product) {
-		try {
-			entityManager.getTransaction().begin();
-			entityManager.persist(product);
-			entityManager.getTransaction().commit();
-		}catch(EntityExistsException e) {
-			LOGGER.log(Level.ERROR, "Product with id {} already in database \n{}", product.getId(),
-					logUtil.getReducedStackTrace(e));
-		}
+		entityManager.getTransaction().begin();
+		entityManager.persist(product);
+		entityManager.getTransaction().commit();
 	}
 }
