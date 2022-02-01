90335e58f007b3ac34be8e2e66ad17d6141a2e7b
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2022-02-01 18:18:29 +0100

    feat(StockMySQLRepository): setup testcontainers database in tests

--- raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/repository/mysql/StockMySQLRepositoryTestcontainersIT.java
+++ raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/repository/mysql/StockMySQLRepositoryTestcontainersIT.java
@@ -1,7 +1,59 @@
 package com.github.raffaelliscandiffio.repository.mysql;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers(disabledWithoutDocker = true)
 class StockMySQLRepositoryTestcontainersIT {
+	private static final String TOTEM_DB_NAME = "totem";
 
+	private static EntityManagerFactory emf;
+	private EntityManager entityManager;
+	private StockMySQLRepository stockRepository;
+
+	@Container
+	public static final MySQLContainer mysqlContainer = new MySQLContainer("mysql:8.0.28")
+			.withDatabaseName(TOTEM_DB_NAME).withUsername("mysql").withPassword("mysql");
+
+
+	@BeforeAll
+	public static void createEntityManagerFactory() {
+		System.setProperty("db.port", mysqlContainer.getFirstMappedPort().toString());
+		emf = Persistence.createEntityManagerFactory("mysql-test");
+	}
+
+	@AfterAll
+	public static void closeEntityManagerFactory() {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setup() {
+		entityManager = emf.createEntityManager();
+		// always starting with empty database
+		entityManager.getTransaction().begin();
+        entityManager.createQuery("DELETE FROM Stock").executeUpdate();
+        entityManager.getTransaction().commit();
+		stockRepository = new StockMySQLRepository(entityManager);
+	}
+
+	@AfterEach
+	void tearDown() {
+		if (entityManager.getTransaction().isActive()) {
+			entityManager.getTransaction().rollback();
+		}
+		if (entityManager.isOpen()) {
+			entityManager.close();
+		}	
+	}
+	
 
 }
--- raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/repository/mysql/StockMySQLRepository.java
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/repository/mysql/StockMySQLRepository.java
@@ -1,9 +1,17 @@
 package com.github.raffaelliscandiffio.repository.mysql;
 
+import javax.persistence.EntityManager;
+
 import com.github.raffaelliscandiffio.model.Stock;
 import com.github.raffaelliscandiffio.repository.StockRepository;
 
 public class StockMySQLRepository implements StockRepository {
+
+	private EntityManager entityManager;
+
+	public StockMySQLRepository(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
 
 	@Override
 	public Stock findById(long id) {
@@ -14,13 +22,13 @@ public class StockMySQLRepository implements StockRepository {
 	@Override
 	public void save(Stock stock) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Stock stock) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
