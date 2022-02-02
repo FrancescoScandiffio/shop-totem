cb17615540f883eef677b2df529cec453c2a1e72
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2022-02-02 11:57:49 +0100

    feat(StockMongoRepository): swap in-memory db to mongo testcontainers

--- raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/repository/mongo/StockMongoRepositoryTestcontainersIT.java
+++ raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/repository/mongo/StockMongoRepositoryTestcontainersIT.java
@@ -2,18 +2,18 @@ package com.github.raffaelliscandiffio.repository.mongo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.bson.Document;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.github.raffaelliscandiffio.model.Stock;
 import com.mongodb.MongoClient;
@@ -21,13 +21,12 @@ import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-import de.bwaldvogel.mongo.MongoServer;
-import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 
+@Testcontainers(disabledWithoutDocker = true)
 class StockMongoRepositoryTestcontainersIT {
-
-	private static MongoServer server;
-	private static InetSocketAddress serverAddress;
+	
+	@Container
+	public static final MongoDBContainer mongo = new MongoDBContainer("mongo:5.0.6");
 
 	private MongoClient client;
 	private StockMongoRepository stockRepository;
@@ -36,21 +35,10 @@ class StockMongoRepositoryTestcontainersIT {
 	private static final String TOTEM_DB_NAME = "totem";
 	private static final String STOCK_COLLECTION_NAME = "stock";
 
-	@BeforeAll
-	public static void setupServer() {
-		server = new MongoServer(new MemoryBackend());
-		// bind on a random local port
-		serverAddress = server.bind();
-	}
-
-	@AfterAll
-	public static void shutdownServer() {
-		server.shutdown();
-	}
 
 	@BeforeEach
 	public void setup() {
-		client = new MongoClient(new ServerAddress(serverAddress));
+		client = new MongoClient(new ServerAddress(mongo.getContainerIpAddress(), mongo.getFirstMappedPort()));
 		stockRepository = new StockMongoRepository(client, TOTEM_DB_NAME, STOCK_COLLECTION_NAME);
 		MongoDatabase database = client.getDatabase(TOTEM_DB_NAME);
 		// make sure we always start with a clean database
@@ -106,5 +94,4 @@ class StockMongoRepositoryTestcontainersIT {
 				.map(d -> new Stock(Long.valueOf("" + d.get("_id")), Integer.valueOf("" + d.get("quantity"))))
 				.collect(Collectors.toList());
 	}
-
 }
