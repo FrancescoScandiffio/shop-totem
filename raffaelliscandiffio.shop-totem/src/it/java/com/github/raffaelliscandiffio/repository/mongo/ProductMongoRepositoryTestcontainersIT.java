a2381697f708b1c5ab43121fdd96b1f47560e358
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2022-02-02 10:43:17 +0100

    feat(ProductMongoRepository): swap in-memory db to Testcontainers Mongo

--- raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/repository/mongo/ProductMongoRepositoryTestcontainersIT.java
+++ raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/repository/mongo/ProductMongoRepositoryTestcontainersIT.java
@@ -3,19 +3,19 @@ package com.github.raffaelliscandiffio.repository.mongo;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.bson.Document;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.github.raffaelliscandiffio.model.Product;
 import com.mongodb.MongoClient;
@@ -23,13 +23,12 @@ import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-import de.bwaldvogel.mongo.MongoServer;
-import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 
+@Testcontainers(disabledWithoutDocker = true)
 class ProductMongoRepositoryTestcontainersIT {
 
-	private static MongoServer server;
-	private static InetSocketAddress serverAddress;
+	@Container
+	public static final MongoDBContainer mongo = new MongoDBContainer("mongo:5.0.6");
 
 	private MongoClient client;
 	private ProductMongoRepository productRepository;
@@ -38,21 +37,9 @@ class ProductMongoRepositoryTestcontainersIT {
 	private static final String TOTEM_DB_NAME = "totem";
 	private static final String PRODUCT_COLLECTION_NAME = "product";
 
-	@BeforeAll
-	public static void setupServer() {
-		server = new MongoServer(new MemoryBackend());
-		// bind on a random local port
-		serverAddress = server.bind();
-	}
-
-	@AfterAll
-	public static void shutdownServer() {
-		server.shutdown();
-	}
-
 	@BeforeEach
 	public void setup() {
-		client = new MongoClient(new ServerAddress(serverAddress));
+		client = new MongoClient(new ServerAddress(mongo.getContainerIpAddress(), mongo.getFirstMappedPort()));
 		productRepository = new ProductMongoRepository(client, TOTEM_DB_NAME, PRODUCT_COLLECTION_NAME);
 		MongoDatabase database = client.getDatabase(TOTEM_DB_NAME);
 		// make sure we always start with a clean database
