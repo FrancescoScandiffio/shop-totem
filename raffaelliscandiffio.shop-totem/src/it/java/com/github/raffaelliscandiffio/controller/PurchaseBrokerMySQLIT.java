3a97c5308c236a5ff8ae490d7cd766802113e18d
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2022-02-05 12:39:46 +0100

    fix: MySQLContainer warning
    Warning: MySQLContainer is a raw type. References to generic type MySQLContainer<SELF> should be parameterized

--- raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/controller/PurchaseBrokerMySQLIT.java
+++ raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/controller/PurchaseBrokerMySQLIT.java
@@ -38,7 +38,7 @@ class PurchaseBrokerMySQLIT {
 	private static final String TOTEM_DB_NAME = "totem";
 	
 	@Container
-	public static final MySQLContainer mysqlContainer = new MySQLContainer("mysql:8.0.28")
+	public static final MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0.28")
 			.withDatabaseName(TOTEM_DB_NAME).withUsername("mysql").withPassword("mysql");
 
 	@BeforeAll
