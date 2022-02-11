dcf679e7a657e173f2679acd39c89d5c36ffc6b9
FrancescoScandiffio <francesco.scandiffio@stud.unifi.it>
2022-02-11 19:07:47 +0100

    break(OrderItem): remove logic and delete related test suite

--- raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/model/OrderItem.java
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/model/OrderItem.java
@@ -1,5 +1,7 @@
 package com.github.raffaelliscandiffio.model;
 
+import java.util.Objects;
+
 public class OrderItem {
 
 	private final String id;
@@ -7,63 +9,29 @@ public class OrderItem {
 	private int quantity;
 	private double subTotal;
 
-	/**
-	 * Constructs a new OrderItem with the specified product and quantity.
-	 * 
-	 * @param product  the product to be purchased
-	 * @param quantity the quantity to be purchased
-	 * @throws NullPointerException     if the specified product is null
-	 * @throws IllegalArgumentException if the specified quantity is non-positive
-	 */
-	public OrderItem(String id, Product product, int quantity) {
-		if (product == null)
-			throw new NullPointerException("Null product");
+	public OrderItem(String id, Product product, int quantity, double subTotal) {
 		this.id = id;
-		handleNonPositiveQuantity(quantity);
 		this.product = product;
 		this.quantity = quantity;
-		updateSubTotal();
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public Product getProduct() {
-		return product;
-	}
-
-	public double getSubTotal() {
-		return subTotal;
-	}
-
-	private void updateSubTotal() {
-		this.subTotal = this.quantity * product.getPrice();
-	}
-
-	private void handleNonPositiveQuantity(int quantity) {
-		if (quantity <= 0)
-			throw new IllegalArgumentException(String.format("Non-positive quantity: (%d)", quantity));
-	}
-
-	void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	void setSubTotal(double subTotal) {
 		this.subTotal = subTotal;
 	}
 
-	void setProduct(Product product) {
-		this.product = product;
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, product, quantity, subTotal);
 	}
 
-	OrderItem(String id) {
-		this.id = id;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id) && Objects.equals(product, other.product) && quantity == other.quantity
+				&& Double.doubleToLongBits(subTotal) == Double.doubleToLongBits(other.subTotal);
 	}
 
 }
