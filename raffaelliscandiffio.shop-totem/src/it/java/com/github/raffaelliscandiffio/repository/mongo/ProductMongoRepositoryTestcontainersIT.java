9cee673e5411eb42235f9bf24286b8ed027e84bb
FrancescoScandiffio <francesco.scandiffio@stud.unifi.it>
2022-06-03 00:24:08 +0200

    ProductMongoRepository: auto generated ObjectId on insert

--- raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/repository/mongo/ProductMongoRepositoryTestcontainersIT.java
+++ raffaelliscandiffio.shop-totem/src/it/java/com/github/raffaelliscandiffio/repository/mongo/ProductMongoRepositoryTestcontainersIT.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import org.assertj.core.api.SoftAssertions;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -30,18 +32,22 @@ class ProductMongoRepositoryTestcontainersIT {
 	private MongoClient client;
 	private ProductMongoRepository productRepository;
 	private MongoCollection<Document> productCollection;
-
 	private static final String TOTEM_DB_NAME = "totem";
 	private static final String PRODUCT_COLLECTION_NAME = "product";
+
+	private Product product_1;
+	private Product product_2;
 
 	@BeforeEach
 	public void setup() {
 		client = new MongoClient(new ServerAddress(mongo.getContainerIpAddress(), mongo.getFirstMappedPort()));
 		productRepository = new ProductMongoRepository(client, TOTEM_DB_NAME, PRODUCT_COLLECTION_NAME);
 		MongoDatabase database = client.getDatabase(TOTEM_DB_NAME);
-		// make sure we always start with a clean database
 		database.drop();
 		productCollection = database.getCollection(PRODUCT_COLLECTION_NAME);
+		product_1 = new Product("pizza", 5.0);
+		product_2 = new Product("pasta", 0.8);
+
 	}
 
 	@AfterEach
@@ -50,57 +56,62 @@ class ProductMongoRepositoryTestcontainersIT {
 	}
 
 	@Test
-	@DisplayName("'findAll' when the database is empty")
+	@DisplayName("Insert product in database with 'save'")
+	void testSaveProduct() {
+		productRepository.save(product_1);
+		String assignedId = product_1.getId();
+
+		SoftAssertions softly = new SoftAssertions();
+		softly.assertThat(assignedId).isNotNull();
+		softly.assertThatCode(() -> new ObjectId(assignedId)).doesNotThrowAnyException();
+		softly.assertThat(readAllProductsFromDatabase()).containsExactly(product_1);
+		softly.assertAll();
+	}
+
+	@Test
+	@DisplayName("Retrieve all the products from the database with 'findAll'")
+	void testFindAllWhenDatabaseIsNotEmpty() {
+		product_1.setId(new ObjectId().toString());
+		product_2.setId(new ObjectId().toString());
+		addTestProductToDatabase(product_1);
+		addTestProductToDatabase(product_2);
+		assertThat(productRepository.findAll()).containsExactlyInAnyOrder(product_1, product_2);
+	}
+
+	@Test
+	@DisplayName("Method 'findAll' should return an empty collection when the database is empty")
 	void testFindAllWhenDatabaseIsEmpty() {
 		assertThat(productRepository.findAll()).isEmpty();
 	}
 
 	@Test
-	@DisplayName("'findAll' when the database is not empty")
-	void testFindAllWhenDatabaseIsNotEmpty() {
-		addTestProductToDatabase("1", "pizza", 5.5);
-		addTestProductToDatabase("2", "pasta", 2.3);
-		assertThat(productRepository.findAll()).containsExactly(createTestProductWithId("1", "pizza", 5.5),
-				createTestProductWithId("2", "pasta", 2.3));
-	}
-
-	@Test
-	@DisplayName("'findById' when the id is not found should return null")
-	void testFindByIdWhenIdIsNotFoundShouldReturnNull() {
-		assertThat(productRepository.findById("1")).isNull();
-	}
-
-	@Test
-	@DisplayName("'findById' when the id is found")
+	@DisplayName("Retrieve Product by id with 'findById'")
 	void testFindByIdWhenIdIsFound() {
-		addTestProductToDatabase("1", "pizza", 5.5);
-		addTestProductToDatabase("2", "pasta", 2.3);
-		assertThat(productRepository.findById("2")).isEqualTo(createTestProductWithId("2", "pasta", 2.3));
+		product_1.setId(new ObjectId().toString());
+		product_2.setId(new ObjectId().toString());
+		addTestProductToDatabase(product_1);
+		addTestProductToDatabase(product_2);
+		assertThat(productRepository.findById(product_1.getId())).isEqualTo(product_1);
 	}
 
 	@Test
-	@DisplayName("'save' product to repository")
-	void testSaveProduct() {
-		Product product = createTestProductWithId("1", "pizza", 5.5);
-		productRepository.save(product);
-		assertThat(readAllProductsFromDatabase()).containsExactly(product);
-	}
-
-	private void addTestProductToDatabase(String id, String name, double price) {
-		productCollection.insertOne(new Document().append("_id", id).append("name", name).append("price", price));
+	@DisplayName("Method 'findById' should return null when the id is not found")
+	void testFindByIdWhenIdIsNotFoundShouldReturnNull() {
+		String missing_id = new ObjectId().toString();
+		assertThat(productRepository.findById(missing_id)).isNull();
 	}
 
 	private List<Product> readAllProductsFromDatabase() {
 		return StreamSupport.stream(productCollection.find().spliterator(), false).map(d -> {
 			Product p = new Product(d.getString("name"), d.getDouble("price"));
-			p.setId(d.getString("_id"));
+			p.setId(d.get("_id").toString());
 			return p;
 		}).collect(Collectors.toList());
 	}
 
-	private Product createTestProductWithId(String id, String name, double price) {
-		Product product = new Product(name, price);
-		product.setId(id);
-		return product;
+	private void addTestProductToDatabase(Product product) {
+		Document productDocument = new Document().append("_id", new ObjectId(product.getId()))
+				.append("name", product.getName()).append("price", product.getPrice());
+		productCollection.insertOne(productDocument);
 	}
 }
--- raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/repository/mongo/ProductMongoRepository.java
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/repository/mongo/ProductMongoRepository.java
@@ -1,11 +1,11 @@
 package com.github.raffaelliscandiffio.repository.mongo;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import com.github.raffaelliscandiffio.model.Product;
 import com.github.raffaelliscandiffio.repository.ProductRepository;
@@ -22,28 +22,31 @@ public class ProductMongoRepository implements ProductRepository {
 	}
 
 	@Override
+	public void save(Product product) {
+		Document productDocument = new Document().append("name", product.getName()).append("price", product.getPrice());
+		productCollection.insertOne(productDocument);
+		product.setId(productDocument.get("_id").toString());
+
+	}
+
+	@Override
 	public List<Product> findAll() {
 		return StreamSupport.stream(productCollection.find().spliterator(), false).map(this::fromDocumentToProduct)
 				.collect(Collectors.toList());
 	}
 
-	private Product fromDocumentToProduct(Document d) {
-		Product p = new Product(d.getString("name"), d.getDouble("price"));
-		p.setId(d.getString("_id"));
-		return p;
-	}
-
 	@Override
-	public Product findById(String id) throws NoSuchElementException {
-		Document d = productCollection.find(Filters.eq("_id", id)).first();
+	public Product findById(String id) {
+		Document d = productCollection.find(Filters.eq("_id", new ObjectId(id))).first();
 		if (d != null)
 			return fromDocumentToProduct(d);
-		return null;
+		else
+			return null;
 	}
 
-	@Override
-	public void save(Product product) {
-		productCollection.insertOne(new Document().append("_id", product.getId()).append("name", product.getName())
-				.append("price", product.getPrice()));
+	private Product fromDocumentToProduct(Document d) {
+		Product p = new Product(d.getString("name"), d.getDouble("price"));
+		p.setId(d.get("_id").toString());
+		return p;
 	}
 }
