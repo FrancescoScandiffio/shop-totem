c250937c9ea3693573207df29546041a0efc9aab
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2022-06-09 16:05:37 +0200

    feat: add service layer interface for UT with controller

---
+++ raffaelliscandiffio.shop-totem/src/main/java/com/github/raffaelliscandiffio/service/ShoppingService.java
@@ -0,0 +1,28 @@
+package com.github.raffaelliscandiffio.service;
+
+import java.util.List;
+
+import com.github.raffaelliscandiffio.model.Order;
+import com.github.raffaelliscandiffio.model.OrderItem;
+import com.github.raffaelliscandiffio.model.Product;
+
+public interface ShoppingService  {
+	
+	
+	public List<Product> getAllProducts();
+	
+	public Order saveOrder(Order order);
+	
+	public void deleteOrder(String orderId);
+
+	public void closeOrder(String orderId, List<OrderItem> orderItems);
+
+	public void deleteItem(OrderItem orderItem);
+
+	public OrderItem returnItem(OrderItem orderItem, int quantityReturned);
+	
+	public List<OrderItem> getOrderItems(String orderId);
+
+	public OrderItem buyProduct(String orderId, String productId, int quantity);
+	
+}
